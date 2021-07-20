ee307d939de1abbe097925c32c8ed08646b33e0b
ahmadreza1383 <61243238+ahmadreza1383@users.noreply.github.com>
2021-07-20 13:09:04 +0430

    Create jalase12.py

---
+++ jalase12.py
@@ -0,0 +1,14 @@
+#///////////////////// foods.items() = pizza and 20000
+
+foods = {'pizza':20000 , 'soosis' :10000 , 'kalbas' : 15000}
+
+foods.items()
+for i , j in foods.items():
+    print(j , i)
+    
+    
+    
+    
+    
+    
+    #//////////////////////////////////////////////////////////
