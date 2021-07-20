e2982a3286dd4f3de251dc2f6e2cc4234430a3ed
ahmadreza1383 <61243238+ahmadreza1383@users.noreply.github.com>
2021-07-20 13:40:50 +0430

    Update jalase12.py

--- jalase12.py
+++ jalase12.py
@@ -38,3 +38,13 @@ for food , money in foods.items():
     if money >= 15000:
         print(food)
 
+        
+        
+        
+#///////////////////// از کانت برای گرفتن تعداد کاراکتر استفاده میکنیم
+v = {'a' : 0 ,'e' : 0 ,'i' : 0 ,'o' : 0 ,'u' : 0 }
+
+sentence = input('inter sentence : ')
+for i , j in v.items():
+    j = sentence.count(i)
+    print(i , j)
