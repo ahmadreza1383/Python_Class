6ee749525417767ee725b44992c761fbf65a6510
ahmadreza1383 <61243238+ahmadreza1383@users.noreply.github.com>
2021-07-20 14:07:40 +0430

    Update jalase12.py

--- jalase12.py
+++ jalase12.py
@@ -50,7 +50,15 @@ for i , j in v.items():
     print(i , j)
     
     
+#///////////////////// استفاده از تارتل برای چاپ یک مربع
+
+import turtle
+
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
     
-    
+
+turtle.done()
     
     
