e5f7b60c894f5473ccfdc7bcb3ab0a7c318227c0
ahmadreza1383 <61243238+ahmadreza1383@users.noreply.github.com>
2021-07-20 13:11:25 +0430

    Update jalase12.py

--- jalase12.py
+++ jalase12.py
@@ -11,4 +11,13 @@ for i , j in foods.items():
     
     
     
-    #//////////////////////////////////////////////////////////
+    
+    #///////////////////// check to money
+    
+    
+    foods = {'pizza':20000 , 'soosis' :10000 , 'kalbas' : 15000}
+
+foods.items()
+for food , money in foods.items():
+    if money >= 15000:
+        print(food)
