1cce0e804ef6fb705cd76f3af6e7f3c82f9487cf
ahmadreza1383 <61243238+ahmadreza1383@users.noreply.github.com>
2021-07-20 13:17:41 +0430

    Update jalase12.py

--- jalase12.py
+++ jalase12.py
@@ -12,12 +12,29 @@ for i , j in foods.items():
     
     
     
-    #///////////////////// check to money
+#///////////////////// check to money
     
     
-    foods = {'pizza':20000 , 'soosis' :10000 , 'kalbas' : 15000}
+foods = {'pizza':20000 , 'soosis' :10000 , 'kalbas' : 15000}
 
 foods.items()
 for food , money in foods.items():
     if money >= 15000:
         print(food)
+        
+        
+        
+        
+        
+#/////////////////////  setdefault   در صورت وجود هیچ کاری نمیکنه و اگه وجود نداشت مقدار رو اضافه میکنه
+        
+        
+foods = {'pizza':20000 , 'soosis' :10000 , 'kalbas' : 15000}
+
+foods.setdefault('pasta' , 20000)
+
+foods.items()
+for food , money in foods.items():
+    if money >= 15000:
+        print(food)
+
