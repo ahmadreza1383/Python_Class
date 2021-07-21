0684b7585c87c11a6c0da45425e694d1671be1bf
ahmadreza <61243238+ahmadreza1383@users.noreply.github.com>
2021-07-21 13:07:39 +0430

    Update Turtle_chess.py

--- session_12/Turtle_chess.py
+++ session_12/Turtle_chess.py
@@ -1,1 +1,58 @@
 
+import turtle
+
+sc = turtle.Screen()
+
+pen = turtle.Turtle()
+
+def draw():
+    for x in range(4):
+        pen.forward(30)
+        pen.left(90)
+    pen.forward(30)
+
+if __name__ == "__main__" :
+      
+       
+    # set turtle object speed
+    pen.speed(100)
+       
+    # loops for board
+    for i in range(8):
+       
+      # not ready to draw
+      pen.up()
+
+      # set position for every row
+      pen.setpos(0, 30 * i)
+
+      # ready to draw
+      pen.down()
+       
+      # row
+      
+      for j in range(8):
+       
+        # conditions for alternative color
+        if (i + j)% 2 == 0:
+          col ='black'
+       
+        else:
+          col ='white'
+       
+        # fill with given color
+        pen.fillcolor(col)
+       
+        # start filling with colour
+        pen.begin_fill()
+       
+        # call method
+        draw()
+        # stop filling
+        pen.end_fill()
+       
+    # hide the turtle
+    pen.hideturtle()
+
+
+turtle.done()
