e492800ab51344d664ea60b631cc81cfecdf16e5
ahmadreza <61243238+ahmadreza1383@users.noreply.github.com>
2021-07-25 13:43:58 +0430

    Create dic_def.py

---
+++ session_13/dic_def.py
@@ -0,0 +1,11 @@
+def display_name():
+    print("hello world")
+    print("alireza")
+    print("parsa")
+    print("amir hossein")
+
+i = input("please inter your function")
+dic = {
+    "display_name":display_name
+}
+dic[i]()
