cce8d76416630f7b18b63ca33ba97029769dd370
ahmadreza <61243238+ahmadreza1383@users.noreply.github.com>
2021-07-27 13:10:22 +0430

    Update display_name.py

--- session_14/display_name.py
+++ session_14/display_name.py
@@ -1,7 +1,8 @@
-def display_nmae(name , name):
-  print(f"hello {name1}")
-  print(f"hello {name2}")
-  
-display_name("abbas" , "jafar")
-display_name("parsa" , "alireza")
-display_name("ahmadreza" , "ilia")
+def display_name(name1, name2):
+    print(f"hello {name1}")
+    print(f"hello {name2}")
+
+
+display_name("abbas", "jafar")
+display_name("parsa", "alireza")
+display_name("ahmadreza", "ilia")
