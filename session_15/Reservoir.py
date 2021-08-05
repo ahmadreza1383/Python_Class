96e5d879ac4165391a8f192f99d94fa942f11a96
ahmadreza <61243238+ahmadreza1383@users.noreply.github.com>
2021-08-05 16:48:21 +0430

    Create Reservoir.py

---
+++ session_15/Reservoir.py
@@ -0,0 +1,10 @@
+width = int(input("width : "))
+length = int(input("length : "))
+Height = int(input("Height : "))
+
+def Reservoir(width , lenght , height):
+   return width * lenght * height
+
+x = Reservoir(width , length , Height)
+
+print(x)
